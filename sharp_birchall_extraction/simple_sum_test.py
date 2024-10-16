7fdd335105cd50819c797a419a53a03963094998
mwanakijiji <eckhart.spalding@sydney.edu.au>
2024-10-17 10:30:23 +1100

    just before trying to split the readouts and profile images in x,y

--- sharp_birchall_extraction/simple_sum_test.py
+++ sharp_birchall_extraction/simple_sum_test.py
@@ -12,11 +12,13 @@ import pandas as pd
 from astropy.io import fits
 import matplotlib.pyplot as plt
 
-stem = '/Users/bandari/Downloads/yoo-jung-smf/'
+#stem = '/Users/bandari/Downloads/yoo-jung-smf/'
+stem = '/Users/bandari/Documents/git.repos/GLINT_reduction_v3/data/sample_data/'
 
-file_list = glob.glob(stem + '*.fits')
+#file_list = glob.glob(stem + '*.fits')
+file_list = glob.glob(stem + 'test_02_slice_008500.fits')
 
-dir_write = stem + 'simple_sum/'
+dir_write = stem + 'junk/'
     
 for file_name in file_list:
 
@@ -24,11 +26,34 @@ for file_name in file_list:
         
         data = hdul[0].data
 
-        spec_1d = np.sum(data[99:108,:], axis=0)
+        spec_1d_00 = np.sum(data[236:241,:], axis=0)
+        spec_1d_01 = np.sum(data[217:223,:], axis=0)
+        spec_1d_02 = np.sum(data[198:204,:], axis=0)
+        spec_1d_03 = np.sum(data[180:185,:], axis=0)
+        spec_1d_04 = np.sum(data[162:167,:], axis=0)
+        spec_1d_05 = np.sum(data[142:147,:], axis=0)
+        spec_1d_06 = np.sum(data[124:128,:], axis=0)
+        spec_1d_07 = np.sum(data[105:109,:], axis=0)
+        spec_1d_08 = np.sum(data[85:90,:], axis=0)
+        spec_1d_09 = np.sum(data[48:52,:], axis=0)
+        spec_1d_10 = np.sum(data[30:33,:], axis=0)
+        spec_1d_11 = np.sum(data[9:14,:], axis=0)
 
         # write plot
         plt.clf()
-        plt.plot(spec_1d, color='r')
+        offset = 50000
+        plt.plot(spec_1d_00 + 0*offset)
+        plt.plot(spec_1d_01 + 1*offset)
+        plt.plot(spec_1d_02 + 2*offset)
+        plt.plot(spec_1d_03 + 3*offset)
+        plt.plot(spec_1d_04 + 4*offset)
+        plt.plot(spec_1d_05 + 5*offset)
+        plt.plot(spec_1d_06 + 6*offset)
+        plt.plot(spec_1d_07 + 7*offset)
+        plt.plot(spec_1d_08 + 8*offset)
+        plt.plot(spec_1d_09 + 9*offset)
+        plt.plot(spec_1d_10 + 10*offset)
+        plt.plot(spec_1d_11 + 11*offset)
         file_write_name_plot = dir_write + os.path.basename(file_name).split('.')[0] + '.png'
         plt.savefig(file_write_name_plot)
         print('Wrote', file_write_name_plot)
